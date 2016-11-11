7bcba9a5900a792b5f91fe7e95def12e4acff49d
Ganesh <lastlegion@gmail.com>
2016-11-11 03:12:48 +0000

    passing all test cases

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 # execute batch loading of batch_payment.txt into Neo4J
 # Please provide absolute path for `f`
 tput setaf 1; echo "Step1: Loading Data, Creating Neo4J Graph"; tput sgr0;
-{ echo "export f=file:///home/ubuntu/InsightPayMO/paymo_input/batch_head.csv"; cat src/batchCreate.cql; } | neo4j-shell
+{ echo "export f=file://"$(echo $(pwd))"/paymo_input/batch_payment.txt"; cat src/batchCreate.cql; } | neo4j-shell
 
 
 #Please note all paths are relative to src directory
@@ -12,17 +12,17 @@ cd src
 echo ""; tput setaf 1; echo "Step2: Executing feature 1"; tput sgr0;
 
 # execute feature 1
-node feature1_.js ../paymo_input/stream_head.csv ../paymo_output/output1.txt
+node feature1_.js ../paymo_input/stream_payment.txt ../paymo_output/output1.txt
 
 
 echo ""; tput setaf 1; echo "Step3: Executing feature 2"; tput sgr0;
 
 # execute feature 2
-node feature2_.js ../paymo_input/stream_head.csv ../paymo_output/output2.txt
+node feature2_.js ../paymo_input/stream_payment.txt ../paymo_output/output2.txt
 
 
 echo""; tput setaf 1; echo "Step4: Executing feature 3"; tput sgr0;
 
 # exectue feature 3
-node feature3_.js ../paymo_input/stream_head.csv ../paymo_output/output3.txt
+node feature3_.js ../paymo_input/stream_payment.txt ../paymo_output/output3.txt
 
